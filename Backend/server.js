e36458a3b46d2f44f23fa00783dc141f30761701
mehfoozkhan03 <mehfooznettech786@gmail.com>
2026-06-22 16:35:43 +0530

    code of the day

---
+++ Backend/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+import { Connection } from './config/db.js';
+// import { blogRoutes } from './Routes/Blog.routes.js';
+import { userRoutes } from './Routes/User.routes.js';
+import { auth } from './middleware/AuthMiddleware.js';
+// import { blogModel } from './model/Blog.model.js';
+
+const server = express();
+
+server.use(express.text(), express.json(), cors());
+
+/* server.get('/', async (req, res) => {
+  const blogs = await blogModel.find().populate('author');
+  res.send(blogs);
+}); */
+
+// routes
+server.use('/user', userRoutes);
+
+// auth-check
+server.use(auth);
+
+// blog routes
+// server.use('/blog', blogRoutes);
+
+// server running
+server.listen(process.env.Port, async () => {
+  try {
+    await Connection();
+    console.log('DB Connected successfully✅');
+  } catch (error) {
+    console.log(`🚀 ~ error:`, error);
+    console.log('DB Crashed! something went wrong ❌');
+  } finally {
+    console.log(`port is running on ${process.env.Port}`);
+  }
+});
