d31b5e617fa2f7a45ca53cc499a75966151fa679
AfanShaikh <shaikhaffan.8286@gmail.com>
2026-06-30 20:43:19 +0530

    updated FAQ section in Contact and maded some minor changes in the CSS of Contact-form,FAQ ,FAQItem

--- timewise-react/src/pages/Contact.jsx
+++ timewise-react/src/pages/Contact.jsx
@@ -1,14 +1,14 @@
 import ContactHero
-from "../components/Contact/ContactHero";
+from "../components/Contact/ContactHero/ContactHero.jsx";
 
 import ContactInfo
-from "../components/Contact/ContactInfo";
+from "../components/Contact/ContactInfo/ContactInfo.jsx";
 
 import ContactForm
-from "../components/Contact/ContactForm";
+from "../components/Contact/ContactForm/ContactForm.jsx";
 
 import FAQ
-from "../components/Contact/FAQ";
+from "../components/Contact/FAQ/FAQ.jsx";
 
 import "../styles/Contact.css";
 
