a0b309ea41345177d37c78bd6fb8c9ae634dfbe3
arnavKharade <arnavkharade423@gmail.com>
2026-06-30 16:28:04 +0530

    Created some stats pages and updated the global css

---
+++ timewise-react/src/components/home/StatsGrid/StatsGrid.jsx
@@ -0,0 +1,46 @@
+import './StatsGrid.css'
+
+import { FaBullseye, FaUserCheck, FaClock, FaFire } from 'react-icons/fa'
+
+import StatCard from '../../StatCard/StatCard'
+
+export default function StatsGrid() {
+  const stats = [
+    {
+      title: 'Productivity',
+      value: '89%',
+      subtitle: '+5% from last week',
+      progress: 89,
+      icon: <FaBullseye />,
+    },
+    {
+      title: 'Attendance',
+      value: '97%',
+      subtitle: 'Excellent',
+      progress: 97,
+      icon: <FaUserCheck />,
+    },
+    {
+      title: 'Weekly Hours',
+      value: '39.2h',
+      subtitle: '98% Goal Completed',
+      progress: 98,
+      icon: <FaClock />,
+    },
+    {
+      title: 'Current Streak',
+      value: '12 Days',
+      subtitle: 'Personal Best',
+      progress: 82,
+      icon: <FaFire />,
+    },
+  ]
+
+  return (
+    <section className="stats_grid">
+      {stats.map((stat) => (
+        <StatCard key={stat.title} {...stat} />
+      ))}
+    </section>
+  )
+}
