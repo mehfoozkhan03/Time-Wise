d8b7a7db9145050ef341c9310036062fc5d63b9a
AfanShaikh <shaikhaffan.8286@gmail.com>
2026-07-03 19:04:51 +0530

    added changes regarding About

---
+++ timewise-react/src/components/About/Skeleton/CTASkeleton.jsx
@@ -0,0 +1,23 @@
+function CTASkeleton() {
+
+    return (
+
+        <div className="ctaSkeleton">
+
+            <div className="sk_award"></div>
+
+            <div className="sk_line cta_title"></div>
+
+            <div className="sk_line cta_text"></div>
+
+            <div className="sk_line cta_text"></div>
+
+            <div className="sk_line cta_text short"></div>
+
+            <div className="sk_button"></div>
+
+        </div>
+    );
+}
+
+export default CTASkeleton;
