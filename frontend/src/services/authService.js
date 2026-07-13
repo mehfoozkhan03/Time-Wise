9870a43ea9c4afd50091a5c58fa785c8f8e5e6bc
yadav-khushi293 <yadavkhushi293@gmail.com>
2026-07-13 16:13:50 +0530

    code

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,15 +1,19 @@
-import api from './api'
+import api from "./api";
 
 export const authService = {
+  signup(data) {
+    return api.post("/user/signup", data);
+  },
+
   login(data) {
-    return api.post('/auth/login', data)
+    return api.post("/user/login", data);
   },
 
   logout() {
-    return api.post('/auth/logout')
+    return api.post("/user/logout");
   },
 
   getCurrentUser() {
-    return api.get('/auth/me')
+    return api.get("/user/me");
   },
-}
+};
