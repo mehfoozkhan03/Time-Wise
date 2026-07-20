d9594837070d7aca2396a51ac6b3009bc1b4a698
AfanShaikh <shaikhaffan.8286@gmail.com>
2026-07-20 23:29:24 +0530

    code of the day

--- frontend/src/components/Common/EmptyState/EmptyState.jsx
+++ frontend/src/components/Common/EmptyState/EmptyState.jsx
@@ -1,39 +1,19 @@
 import "./EmptyState.css";
 
 export default function EmptyState({
+  icon,
 
-    icon,
+  title = "No Data",
 
-    title = "No Data",
-
-    description = "Nothing to display.",
-
+  description = "Nothing to display.",
 }) {
+  return (
+    <div className="emptyState">
+      <div className="emptyIcon">{icon}</div>
 
-    return (
+      <h3>{title}</h3>
 
-        <div className="emptyState">
-
-            <div className="emptyIcon">
-
-                {icon}
-
-            </div>
-
-            <h3>
-
-                {title}
-
-            </h3>
-
-            <p>
-
-                {description}
-
-            </p>
-
-        </div>
-
-    );
-
+      <p>{description}</p>
+    </div>
+  );
 }
