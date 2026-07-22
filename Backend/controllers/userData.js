5c7423f6775fd2fd79b7d9a855a61ce758762661
arnavKharade <arnavkharade423@gmail.com>
2026-07-22 18:26:20 +0530

    Code of the day

--- Backend/controllers/userData.js
+++ Backend/controllers/userData.js
@@ -309,9 +309,9 @@ export const updateTheme = async (req, res) => {
 export const logout = async (req, res) => {
   try {
     res.clearCookie('token', {
-      httpOnly: false, // use the same value as login
-      secure: false, // true in production with HTTPS
-      sameSite: 'Lax', // same as login
+      httpOnly: false, 
+      secure: false,
+      sameSite: 'Lax',
     });
 
     return res.status(200).json({
