24d9e4723e84c16fd7bfb0d2b8c2168b09042a46
mehfoozkhan03 <mehfooznettech786@gmail.com>
2026-07-22 13:16:25 +0530

    code of the day

--- frontend/src/components/home/QuickActions/ActionCard.jsx
+++ frontend/src/components/home/QuickActions/ActionCard.jsx
@@ -1,21 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-import "./ActionCard.css";
-import Card from "../../Card/Card";
-import { FaArrowRight } from "react-icons/fa";
+import './ActionCard.css';
+import Card from '../../Card/Card';
+import { FaArrowRight } from 'react-icons/fa';
 
-<<<<<<< HEAD
-export default function ActionCard({id, icon, title, description, to }) {
-  console.log("🚀 ~ ActionCard ~ data_id:", id)
-  const navigate = useNavigate()
-=======
-export default function ActionCard({ icon, title, description, to }) {
+export default function ActionCard({ id, icon, title, description, to }) {
   const navigate = useNavigate();
->>>>>>> 0623fba53cdf1a9ff7f23d796e88b3bf1df577de
 
   return (
     <Card className="action_card" onClick={() => navigate(to)}>
-      <div data-id={id} className="action_icon">{icon}</div>
+      <div data-id={id} className="action_icon">
+        {icon}
+      </div>
 
       <h3>{title}</h3>
 
