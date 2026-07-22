5aa0e6962b947c6f775119583b5e361527f48ced
Nilesh Yadav <ny033222@gmail.com>
2026-07-22 17:15:27 +0530

    Code

--- Backend/controllers/userData.js
+++ Backend/controllers/userData.js
@@ -260,11 +260,6 @@ export const getCurrentUser = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie("token", {
-      httpOnly: false,   // use the same value as login
-      secure: false,    // true in production with HTTPS
-      sameSite: "Lax",  // same as login
-    });
 
     return res.status(200).json({
       success: true,
