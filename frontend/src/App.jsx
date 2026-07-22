4b393c78f8096baabe4a2c6dbbf7b07a0ecaa76e
hetuk2005 <patelhetuk2005@gmail.com>
2026-07-22 16:48:20 +0530

    code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter } from 'react-router-dom'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
-import { AppRoutes } from './routes/AppRoutes'
-import { fetchCurrentUser } from './store/authSlice'
+import { AppRoutes } from "./routes/AppRoutes";
+import { fetchCurrentUser } from "./store/authSlice";
 
 export default function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchCurrentUser())
-  }, [dispatch])
+    dispatch(fetchCurrentUser());
+  }, [dispatch]);
 
   return (
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
-  )
+  );
 }
