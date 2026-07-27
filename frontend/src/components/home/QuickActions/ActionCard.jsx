85a33d19ad7e5dba4012146a44113a594e5ed24b
hetuk2005 <patelhetuk2005@gmail.com>
2026-07-27 16:19:03 +0530

    Code

--- frontend/src/components/home/QuickActions/ActionCard.jsx
+++ frontend/src/components/home/QuickActions/ActionCard.jsx
@@ -1,14 +1,53 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
-import './ActionCard.css';
-import Card from '../../Card/Card';
-import { FaArrowRight } from 'react-icons/fa';
+import "./ActionCard.css";
+import Card from "../../Card/Card";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function ActionCard({ id, icon, title, description, to }) {
   const navigate = useNavigate();
 
+  const cardRef = useRef(null);
+
+  const handleMouseMove = (e) => {
+    const card = cardRef.current;
+    if (!card) return;
+
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateY = ((x - centerX) / centerX) * 8;
+    const rotateX = -((y - centerY) / centerY) * 8;
+
+    card.style.setProperty("--rotateX", `${rotateX}deg`);
+    card.style.setProperty("--rotateY", `${rotateY}deg`);
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+
+  const resetTilt = () => {
+    const card = cardRef.current;
+    if (!card) return;
+
+    card.style.setProperty("--rotateX", "0deg");
+    card.style.setProperty("--rotateY", "0deg");
+  };
+
   return (
-    <Card className="action_card" onClick={() => navigate(to)}>
+    <Card
+      ref={cardRef}
+      className="action_card"
+      onClick={() => navigate(to)}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={resetTilt}
+    >
       <div data-id={id} className="action_icon">
         {icon}
       </div>
