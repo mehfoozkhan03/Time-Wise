4a106d6112bb241cedb97e2e96cdbdf727f31ebc
hetuk2005 <patelhetuk2005@gmail.com>
2026-07-29 23:10:03 +0530

    Code

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import { logout } from "../../store/authSlice";
 import { authService } from "../../services/authService";
 import { useTheme } from "../../context/ThemeContext";
 import { fetchNotifications } from ".././../store/notificationSlice";
+import { Feedback } from "../../pages/FeedBack";
 
 export default function Navbar() {
   const { notifications, loading } = useSelector((state) => state.notification);
@@ -24,12 +25,15 @@ export default function Navbar() {
   const [mobileOpen, setMobileOpen] = useState(false);
   const [profileOpen, setProfileOpen] = useState(false);
   const [notificationOpen, setNotificationOpen] = useState(false);
+  const [logoutOpen, setLogoutOpen] = useState(false);
 
   const notificationRef = useRef(null);
   const profileRef = useRef(null);
 
-  const handleLogout = async () => {
+  const confirmLogout = async () => {
     try {
+      setLogoutOpen(false);
+
       await authService.logout();
 
       dispatch(logout());
@@ -38,6 +42,30 @@ export default function Navbar() {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const getLogoutContent = () => {
+    const today = new Date().getDay();
+
+    const firstName =
+      user?.firstName?.charAt(0).toUpperCase() + user?.firstName?.slice(1) ||
+      "User";
+
+    if (today >= 1 && today <= 4) {
+      return {
+        title: `Goodbye, ${firstName}! `,
+        message:
+          "That's a wrap for today! Great work. Take some time to relax and recharge — We'll be ready for another productive day tomorrow.",
+        button: "Logout & Relax",
+      };
+    }
+
+    return {
+      title: `Goodbye, ${firstName}! 👋`,
+      message:
+        "You've wrapped up another productive week. Enjoy your weekend, relax, and come back refreshed. We'll see you on Monday!",
+      button: "Start My Weekend",
+    };
   };
 
   // Close everything on route change
@@ -75,63 +103,64 @@ export default function Navbar() {
   }, [dispatch]);
 
   return (
-    <header className="navbar">
-      {/* =======================
+    <>
+      <header className="navbar">
+        {/* =======================
             Logo
       ======================= */}
 
-      <div className="navbar_logo" id="tour-logo">
-        <NavLink to="/">
-          <div className="logo_text">
-            <img
-              src={
-                resolvedTheme === "dark" ? "/Logo_N.svg" : "/Logo_N_Light.svg"
-              }
-              alt="Logo"
-            />
-          </div>
-        </NavLink>
-      </div>
+        <div className="navbar_logo" id="tour-logo">
+          <NavLink to="/">
+            <div className="logo_text">
+              <img
+                src={
+                  resolvedTheme === "dark" ? "/Logo_N.svg" : "/Logo_N_Light.svg"
+                }
+                alt="Logo"
+              />
+            </div>
+          </NavLink>
+        </div>
 
-      {/* =======================
+        {/* =======================
             Navigation
       ======================= */}
 
-      <nav
-        className={`navbar_links ${mobileOpen ? "active" : ""}`}
-        id="tour-nav-links"
-      >
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/dashboard">Dashboard</NavLink>
-        <NavLink to="/community">Community</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-      </nav>
+        <nav
+          className={`navbar_links ${mobileOpen ? "active" : ""}`}
+          id="tour-nav-links"
+        >
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="/community">Community</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
 
-      {/* =======================
+        {/* =======================
             Right Side
       ======================= */}
 
-      <div className="navbar_right">
-        {/* Notification */}
+        <div className="navbar_right">
+          {/* Notification */}
 
-        <div
-          className="notification_container"
-          id="tour-notifications"
-          ref={notificationRef}
-        >
-          <button
-            className="notification_btn"
-            onClick={() => {
-              setProfileOpen(false);
-              setNotificationOpen((prev) => !prev);
-            }}
+          <div
+            className="notification_container"
+            id="tour-notifications"
+            ref={notificationRef}
           >
-            <FaBell />
-            <span className="notification_count">{notifications.length}</span>
-          </button>
+            <button
+              className="notification_btn"
+              onClick={() => {
+                setProfileOpen(false);
+                setNotificationOpen((prev) => !prev);
+              }}
+            >
+              <FaBell />
+              <span className="notification_count">{notifications.length}</span>
+            </button>
 
-          {/* {notificationOpen && (
+            {/* {notificationOpen && (
             <div className="notification_dropdown">
               <h4>Notifications</h4>
 
@@ -149,106 +178,111 @@ export default function Navbar() {
             </div>
           )} */}
 
-          {notificationOpen && (
-  <div className="notification_dropdown">
-    <h4>Notifications</h4>
+            {notificationOpen && (
+              <div className="notification_dropdown">
+                <h4>Notifications</h4>
 
-    {loading ? (
-      <div className="notification_item">
-        Loading...
-      </div>
-    ) : notifications.length === 0 ? (
-      <div className="notification_item">
-        No notifications found.
-      </div>
-    ) : (
-      notifications.map((notification) => (
-        <div
-          key={notification._id}
-          className="notification_item"
-        >
-          <strong>{notification.title}</strong>
+                {loading ? (
+                  <div className="notification_item">Loading...</div>
+                ) : notifications.length === 0 ? (
+                  <div className="notification_item">
+                    No notifications found.
+                  </div>
+                ) : (
+                  notifications.map((notification) => (
+                    <div key={notification._id} className="notification_item">
+                      <strong>{notification.title}</strong>
 
-          <p>{notification.message}</p>
-        </div>
-      ))
-    )}
+                      <p>{notification.message}</p>
+                    </div>
+                  ))
+                )}
 
-    <button className="view_all_btn">
-      View All
-    </button>
-  </div>
-)}
-        </div>
+                <button className="view_all_btn">View All</button>
+              </div>
+            )}
+          </div>
 
-        {/* Profile */}
+          {/* Profile */}
 
-        <div className="profile_container" id="tour-profile" ref={profileRef}>
-          <button
-            className="profile_btn"
-            onClick={() => {
-              setNotificationOpen(false);
-              setProfileOpen((prev) => !prev);
-            }}
-          >
-            <div className="avatar">
-              {user
-                ? `${user.firstName?.[0] ?? ""}${user.lastName?.[0] ?? ""}`.toUpperCase()
-                : "U"}
-            </div>
-
-            <div className="profile_info">
-              <h4>
+          <div className="profile_container" id="tour-profile" ref={profileRef}>
+            <button
+              className="profile_btn"
+              onClick={() => {
+                setNotificationOpen(false);
+                setProfileOpen((prev) => !prev);
+              }}
+            >
+              <div className="avatar">
                 {user
-                  ? `${user.firstName
-                      ?.charAt(0)
-                      .toUpperCase()}${user.firstName?.slice(1)} ${
-                      user.lastName?.charAt(0).toUpperCase() +
-                      user.lastName?.slice(1)
-                    }`
-                  : "User"}
-              </h4>
+                  ? `${user.firstName?.[0] ?? ""}${user.lastName?.[0] ?? ""}`.toUpperCase()
+                  : "U"}
+              </div>
 
-              <span>Frontend Developer</span>
-            </div>
+              <div className="profile_info">
+                <h4>
+                  {user
+                    ? `${user.firstName
+                        ?.charAt(0)
+                        .toUpperCase()}${user.firstName?.slice(1)} ${
+                        user.lastName?.charAt(0).toUpperCase() +
+                        user.lastName?.slice(1)
+                      }`
+                    : "User"}
+                </h4>
 
-            <FaChevronDown
-              className={`profile_arrow ${profileOpen ? "rotate" : ""}`}
-            />
+                <span>Frontend Developer</span>
+              </div>
+
+              <FaChevronDown
+                className={`profile_arrow ${profileOpen ? "rotate" : ""}`}
+              />
+            </button>
+
+            {profileOpen && (
+              <div className="profile_dropdown">
+                <NavLink to="/employee" onClick={() => setProfileOpen(false)}>
+                  My Profile
+                </NavLink>
+
+                <NavLink to="/settings" onClick={() => setProfileOpen(false)}>
+                  Settings
+                </NavLink>
+
+                <button
+                  onClick={() => {
+                    setProfileOpen(false);
+                    setLogoutOpen(true);
+                  }}
+                >
+                  Logout
+                </button>
+              </div>
+            )}
+          </div>
+
+          {/* Mobile Menu */}
+
+          <button
+            id="tour-mobile-btn"
+            className="mobile_btn"
+            onClick={() => setMobileOpen((prev) => !prev)}
+          >
+            {mobileOpen ? <FaTimes /> : <FaBars />}
           </button>
-
-          {profileOpen && (
-            <div className="profile_dropdown">
-              <NavLink to="/employee" onClick={() => setProfileOpen(false)}>
-                My Profile
-              </NavLink>
-
-              <NavLink to="/settings" onClick={() => setProfileOpen(false)}>
-                Settings
-              </NavLink>
-
-              <button
-                onClick={async () => {
-                  setProfileOpen(false);
-                  await handleLogout();
-                }}
-              >
-                Logout
-              </button>
-            </div>
-          )}
         </div>
+      </header>
 
-        {/* Mobile Menu */}
-
-        <button
-          id="tour-mobile-btn"
-          className="mobile_btn"
-          onClick={() => setMobileOpen((prev) => !prev)}
-        >
-          {mobileOpen ? <FaTimes /> : <FaBars />}
-        </button>
-      </div>
-    </header>
+      <Feedback
+        isOpen={logoutOpen}
+        variant="logout"
+        title={getLogoutContent().title}
+        message={getLogoutContent().message}
+        confirmText={getLogoutContent().button}
+        cancelText="Stay Logged In"
+        onClose={() => setLogoutOpen(false)}
+        onConfirm={confirmLogout}
+      />
+    </>
   );
 }
