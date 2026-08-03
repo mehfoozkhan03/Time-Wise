cfee4e9d0deff7efc748e6e1339adfb9c4d0120c
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-08-03 12:16:54 +0530

    Updated code for notification

--- frontend/src/store/notificationSlice.js
+++ frontend/src/store/notificationSlice.js
@@ -1,47 +1,4 @@
-// import { createSlice } from '@reduxjs/toolkit'
-
-// const initialState = {
-//   notifications: [],
-// }
-
-// const notificationSlice = createSlice({
-//   name: 'notification',
-
-//   initialState,
-
-//   reducers: {
-//     setNotifications(state, action) {
-//       state.notifications = action.payload
-//     },
-
-//     addNotification(state, action) {
-//       state.notifications.unshift(action.payload)
-//     },
-
-//     removeNotification(state, action) {
-//       state.notifications = state.notifications.filter(
-//         (item) => item._id !== action.payload,
-//       )
-//     },
-
-//     clearNotifications(state) {
-//       state.notifications = []
-//     },
-//   },
-// })
-
-// export const {
-//   setNotifications,
-//   addNotification,
-//   removeNotification,
-//   clearNotifications,
-// } = notificationSlice.actions
-
-// export default notificationSlice.reducer
-
-
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-// import { notificationService } from "../services/notificationServices";
 import { getNotifications } from "../services/notificationServices";
 
 // ======================================================
