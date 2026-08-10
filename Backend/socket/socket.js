3373e846ad360dcb29d24668f85e0b60b3003fc5
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-08-10 17:43:14 +0530

    Code of the day

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -11,7 +11,7 @@ export const initializeSocket = (httpServer) => {
       origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        "http://localhost:9000",
+        "http://localhost:8000",
       ],
       credentials: true,
     },
