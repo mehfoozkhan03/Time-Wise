39defc3be7504e54e13561137231e1c9ead60011
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2026-09-09 15:52:22 +0530

    Added Admin Private route

--- frontend/src/components/AdminPrivateRoutes.jsx
+++ frontend/src/components/AdminPrivateRoutes.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { Navigate } from "react-router-dom";
+
+// export const AdminPrivateRoutes = ({ children }) => {
+//   const { isAuthenticated, admin, isLoading } = useSelector(
+//     (state) => state.adminAuth
+//   );
+
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (!isAuthenticated || !admin) {
+//     return <Navigate to="/admin/login" replace />;
+//   }
+
+//   return children;
+// };
+
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 export const AdminPrivateRoutes = ({ children }) => {
-  const { isAuthenticated, admin, isLoading } = useSelector(
-    (state) => state.adminAuth
-  );
+  const { isAuthenticated } = useSelector((state) => state.adminAuth);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  console.log("🚀 ~ admin isAuthenticated:", isAuthenticated);
 
-  if (!isAuthenticated || !admin) {
-    return <Navigate to="/admin/login" replace />;
+  if (
+    !isAuthenticated ||
+    isAuthenticated === undefined ||
+    isAuthenticated === null
+  ) {
+    return (
+      <Navigate
+        to="/admin/login"
+        replace
+      />
+    );
   }
 
   return children;
