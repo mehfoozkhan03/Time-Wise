d9bc22550f630b0aa33df66074b8ace63e42bc2e
yadav-khushi293 <yadavkhushi293@gmail.com>
2026-09-20 17:47:10 +0530

    code

--- frontend/src/data/form.js
+++ frontend/src/data/form.js
@@ -1,8 +1,72 @@
-const login = [];
+const login = [
+  {
+    id:1,
+    name: "Email",
+    type: "text",
+    placeholder: "Enter you'r Email...",
+  },
+  {
+    id:2,
+    name: "Password",
+    type: "text",
+    placeholder: "Enter you'r Password...",
+  },
+];
 
-const signup = [];
+const signup = [
+{
+    id: 1,
+    name: "First Name",
+    type: "text",
+    placeholder: "Enter you'r first Name...",
+  },
+   {
+    id: 2,
+    name: "Last Name",
+    type: "text",
+    placeholder: "Enter you'r Last Name...",
+  },
+   {
+    id: 3,
+    name: "Email",
+    type: "text",
+    placeholder: "Enter you'r Email...",
+  },
+  {
+    id: 4,
+    name: "Password",
+    type: "text",
+    placeholder: "Enter you'r Password...",
+  },
 
-const admin = [];
+   {
+    id: 5,
+    name: "Confirm Password",
+    type: "text",
+    placeholder: "Enter you'r Confirm Password...",
+  },
+
+   {
+    id: 6,
+    name: "dob",
+    type: "date",
+  },
+];
+
+const admin = [
+  {
+    id: 1,
+    name: "Email",
+    type: "text",
+    placeholder: "Enter you'r Email...",
+  },
+  {
+    id: 1,
+    name: "Password",
+    type: "text",
+    placeholder: "Enter you'r Password...",
+  },
+];
 
 /* 
  {
