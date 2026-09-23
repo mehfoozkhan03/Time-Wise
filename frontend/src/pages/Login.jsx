00ef3bba1b0bcfa6d89153bb552172c4c471ad33
yadav-khushi293 <yadavkhushi293@gmail.com>
2026-09-23 12:54:26 +0530

    cdoe

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Form } from "../components/Form";
-import { forms } from "../data/form";
-
+// import { Form } from "../components/Form";
+// import { forms } from "../data/form";
+import AuthForm from "../components/AuthForm";
 
 
 export const Login = () => {
     return (
-        <Form
-           {...forms?.login} 
-        />
+        <>
+        {/* <Form {...forms?.login} /> */}
+        <AuthForm />
+        </>
     );
 };
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,11 +1,18 @@
+// import React from "react";
+// import { Form } from "../components/Form";
+// import { forms } from "../data/form"
+
+// export const Signup = () => {
+//     return (
+//         <Form
+//             {...forms?.signup}
+//         />
+//     );
+// };
+
 import React from "react";
-import { Form } from "../components/Form";
-import { forms } from "../data/form"
+import AuthForm from "../components/AuthForm";
 
 export const Signup = () => {
-    return (
-        <Form
-            {...forms?.signup}
-        />
-    );
+  return <AuthForm />;
 };
